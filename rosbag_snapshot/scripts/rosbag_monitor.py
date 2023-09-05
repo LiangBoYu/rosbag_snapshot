1e700efc96d82bbf007eaf83464466b9c01f8165
LIANGBoYu <s0960246725@gmail.com>
2023-09-06 00:43:42 +0800

    20230906 1. for kinetic版本    只有在特定alarm code才會記錄rosbag 2.修改顯示failed說明

--- rosbag_snapshot/scripts/rosbag_monitor.py
+++ rosbag_snapshot/scripts/rosbag_monitor.py
@@ -13,12 +13,12 @@ from nav_msgs.msg import Odometry
 
 class rosbag_monitor(object):
     def __init__(self):
-        rospy.loginfo("rosbag_monitor_v_0_1_3 start")
+        rospy.loginfo("rosbag_monitor_v_0_1_4 start")
         self.errorsummary = ""
         self.has_alarm_flag = False
         self.latest_odom = None
         self.minus_time = 0
-        self.savepath = '/home/gyro/catkin_ws/src/rosbag_snapshot/bags/'
+        self.savepath = '/home/gyro/maintain_work_logs/rosbag/'
         self.yamlfile = '/home/gyro/catkin_ws/src/rosbag_snapshot/rosbag_snapshot/param/snapshot.yaml'
         self.nowsec = rospy.get_time()
         self.syserr_sub = rospy.Subscriber("/sysindicator/error", syserr, self.syserr_cb)
@@ -28,33 +28,45 @@ class rosbag_monitor(object):
     def syserr_cb(self, msg):
         self.errorsummary = msg.errorsummary
         if self.errorsummary != "" :
-            self.set_minus()
-            if self.has_alarm_flag == False:
-                self.has_alarm_flag = True
-                rospy.loginfo("ALARM_CODE is " + self.errorsummary)
-                now = datetime.now()
-                file_time = now.strftime("%Y%m%d_%H%M%S_")
-                alarm_name = str(self.errorsummary)
-                self.final_file_name = self.savepath +file_time + alarm_name + '_'
-                self.set_start_time = max(self.nowsec - self.minus_time + 180 , 0) #start為發生alarm當下前3分鐘
-                self.set_stop_time = self.set_start_time + 120 #stop為發生alarm後2分鐘
-                self.start_time = rospy.Time(secs=self.set_start_time, nsecs=0)
-                self.stop_time = rospy.Time(secs=self.set_stop_time, nsecs=0) 
-                with open(self.yamlfile, 'r') as rosbag_param:      
-                    data = yaml.safe_load(rosbag_param) 
-                    self.topics = data['topics']
-                rospy.loginfo("final_file_name:{}".format(self.final_file_name))
-                rospy.loginfo("topics:{}".format(self.topics))
-                #rospy.loginfo("minus_time:{}".format(self.minus_time))
-                rospy.loginfo("start_time:{}".format(self.start_time))
-                rospy.loginfo("stop_time:{}".format(self.stop_time))
-                success, message = self.call_trigger_snapshot(self.final_file_name, self.topics, self.start_time, self.stop_time)
-                if success:
-                    rospy.loginfo("Service call successful.")
+
+            with open(self.yamlfile, 'r') as set_alarm_time:
+                try:
+                    self.config = yaml.safe_load(set_alarm_time)
+                except yaml.YAMLError as exc:
+                    rospy.loginfo(exc)
+            for alarm in self.config['alarm_code']:
+                if self.errorsummary in alarm:
+                    self.minus_time = alarm[self.errorsummary]
+
+                    if self.has_alarm_flag == False:
+                        self.has_alarm_flag = True
+                        rospy.loginfo("ALARM_CODE is " + self.errorsummary)
+                        now = datetime.now()
+                        file_time = now.strftime("%Y%m%d_%H%M%S_")
+                        alarm_name = str(self.errorsummary)
+                        self.final_file_name = self.savepath +file_time + alarm_name + '_'
+                        self.set_start_time = max(self.nowsec - self.minus_time + 180 , 0) #start為發生alarm當下前3分鐘
+                        self.set_stop_time = self.set_start_time + 120 #stop為發生alarm後2分鐘
+                        self.start_time = rospy.Time(secs=self.set_start_time, nsecs=0)
+                        self.stop_time = rospy.Time(secs=self.set_stop_time, nsecs=0) 
+                        with open(self.yamlfile, 'r') as rosbag_param:      
+                            data = yaml.safe_load(rosbag_param) 
+                            self.topics = data['topics']
+                        rospy.loginfo("final_file_name:{}".format(self.final_file_name))
+                        rospy.loginfo("topics:{}".format(self.topics))
+                        #rospy.loginfo("minus_time:{}".format(self.minus_time))
+                        rospy.loginfo("start_time:{}".format(self.start_time))
+                        rospy.loginfo("stop_time:{}".format(self.stop_time))
+                        success, message = self.call_trigger_snapshot(self.final_file_name, self.topics, self.start_time, self.stop_time)
+                        if success:
+                            rospy.loginfo("Service call successful.")
+                        else:
+                            rospy.loginfo("Service call failed: {}".format(message))
+                    else:
+                        rospy.loginfo("MR in ALARM, alarm code is {} ,not reset".format(self.errorsummary))
+
                 else:
-                    rospy.loginfo("Service call failed:", message)
-            else:
-                rospy.loginfo("MR in ALARM, alarm code is {} ,not reset".format(self.errorsummary))
+                    rospy.loginfo("dont neer record")
 
         if self.errorsummary == "":
             print("MR OK")
